bbb9b9a2cd2d419cdf999cdab48d330af2b50f8e
santuon@yahoo.com <santuon@yahoo.com>
2017-04-13 23:54:15 +0000

    Added styles and home and about page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,7 +20,7 @@ class ArticlesController < ApplicationController
         @article.word_count = 100
         
         if @article.save
-            flash[:notice] = "Article was successfully created"
+            flash[:success] = "Article was successfully created"
             redirect_to article_path(@article)
         else
             render 'new' 
@@ -35,7 +35,7 @@ class ArticlesController < ApplicationController
 
     def update
         if @article.update(article_params)
-            flash[:notice] = "Article was successfully updated"
+            flash[:success] = "Article was successfully updated"
             redirect_to article_path(@article)
         else
             render 'edit'
@@ -44,7 +44,7 @@ class ArticlesController < ApplicationController
 
     def destroy
         @article.destroy
-        flash[:notice] = "Article was successfully deleted"
+        flash[:success] = "Article was successfully deleted"
         redirect_to articles_path
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'welcome/index'
-  get 'about', to: 'pages#about'
+  get 'pages/home', to: "pages#home"
+  get 'pages/about', to: 'pages#about'
   
   resources :articles
   
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

5226854ed66b1b117c1262fa6500b7b6a6884a63
santuon@yahoo.com <santuon@yahoo.com>
2017-04-17 09:08:08 +0000

    Added admin status on user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170414013313) do
+ActiveRecord::Schema.define(version: 20170417080951) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20170414013313) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end

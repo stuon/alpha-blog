e048f3f9291eb579041629f77f06d0257c18d7b3
santuon@yahoo.com <santuon@yahoo.com>
2017-04-19 12:33:22 +0000

    Added categories and unit test

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   resources :articles
   resources :users, except: [:new]
   
+  resources :categories, except: [:destroy]
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170417080951) do
+ActiveRecord::Schema.define(version: 20170419114305) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -29,6 +34,12 @@ ActiveRecord::Schema.define(version: 20170417080951) do
     t.integer  "word_count"
     t.text     "language_code"
     t.integer  "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
